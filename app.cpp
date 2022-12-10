e2006683e61416d8eb153c679979c628db685236
BADAH Bilal <bilalbadah@gmail.com>
2022-12-10 08:53:01 +0100

    Episode 005 : Explaining some words and concepts in Source Code

--- app.cpp
+++ app.cpp
@@ -1,7 +1,19 @@
+// => This file named Source File
+
+
+// These lines names Source Code
+
+// # this symbol or line means Pre-processor Statement
+// "Include" is a Pre-processor Directive, for more informatio search about C++ pre-processors Directives List
+// IOSTREAM is Library that Contains functions to implement them if you want
 #include <iostream> 
 
+// Main function is the entry point of the program in c++ and many other languages
 int main() { 
     
-    std::cout << "Iam Dragon"; 
+    // run sequentially. line after line
+    std::cout << "Line 1\n"; 
+    std::cout << "Line 2\n"; 
+    std::cout << "Line 3\n"; 
     return 0;
 }
